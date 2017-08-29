43e4abd1d491aad34f09bb59c3989a536635df6d
李云龙 <873825813@qq.com>
2017-08-29 11:02:57 +0800

    Initial commit

---
+++ app/src/main/java/com/liyunlong/snowfall/demo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.liyunlong.snowfall.demo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author liyunlong
+ * @date 2017/8/28 16:43
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_snowfall);
+    }
+}
